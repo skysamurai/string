7671a62d3c1e1fe970e9a23c8678de1056d1cf40
Shona Mirna <smirna@student.21-school.ru>
2021-11-01 16:19:12 +0700

    3th target

--- src/s21_string.c
+++ src/s21_string.c
@@ -28,14 +28,10 @@ void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
 }
 
 void *s21_memmove(void *dest, const void *src, s21_size_t n) {
-    char* temp;
+    void* temp;
     temp = malloc(n*sizeof(char));
-    for (s21_size_t i = 0; i < n; i++) {
-        *(temp + i) = *((char*)src + i);
-    }
-    for (s21_size_t i = 0; i < n; i++) {
-        *((char*)dest + i) = *(temp + i);
-    }
+    s21_memcpy(temp, src, n);
+    s21_memcpy(dest, temp, n);
     free(temp);
     return dest;
 }
@@ -45,4 +41,97 @@ void *s21_memset(void *str, int c, s21_size_t n) {
         *((char*)str + i) = c;
     }
     return str;
+}
+
+ char *s21_strchr(const char *str, int c) {
+    int i = 0;
+    while ((*(str + i) != '\0') & ((*(str + i) != c))) {
+        i++;
+    }
+    return (char*)str + i;
+}
+
+s21_size_t s21_strlen(const char *str) {
+    return (s21_strchr(str, 0) - str);
+}
+
+void *s21_to_upper(const char *str) {
+    int n = s21_strlen(str);
+    void* temp = malloc(n * sizeof(char));
+    for (int i = 0; i < n; i++) {
+        if ((*((char*)str + i) > 96) & (*((char*)str + i) < 123)) {
+            *((char*)temp + i) = *((char*)str + i) - 32;
+        } else {
+            *((char*)temp + i) = *((char*)str + i);
+        }
+    }
+    return (char*)temp;
+}
+
+void *s21_to_lower(const char *str) {
+    int n = s21_strlen(str);
+    void* temp = malloc(n * sizeof(char));
+    for (int i = 0; i < n; i++) {
+        if ((*((char*)str + i) > 64) & (*((char*)str + i) < 91)) {
+            *((char*)temp + i) = *((char*)str + i) + 32;
+        } else {
+            *((char*)temp + i) = *((char*)str + i);
+        }
+    }
+    return (char*)temp;
+}
+
+char *s21_strcpy(char *dest, const char *src) {
+    s21_size_t n = s21_strlen(src);
+    s21_memcpy(dest, src, n);
+    return dest;
+}
+
+void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
+    s21_size_t n_src = s21_strlen(src), n_str = s21_strlen(str);
+    void* temp;
+    if (start_index > n_src - 1) {
+        temp = NULL;
+    } else {
+        int n = n_src + n_str;  
+        temp = malloc(n * sizeof(char)); 
+        s21_memcpy(temp, (char*)src, start_index);
+        printf ("func: %s\n",str);
+        s21_memcpy((char*)(temp + start_index), (char*)str, n_str);
+        s21_memcpy(((char*)temp + start_index + n_str), ((char*)src + start_index), n_src - start_index);
+    }
+    return temp;
+}
+
+char *s21_strpbrk(const char *str1, const char *str2) {
+    char *returnVal = NULL;
+    int i = 0;
+    while (str1[i] != '\0' && returnVal == NULL) {
+        for (int j = 0; str2[j] != '\0'; j++) {
+            if (str1[i] == str2[j]) {
+                returnVal = ((char *)str1) + i;
+                break;
+            }
+        }
+        i++;
+    }
+    return returnVal;
+}
+
+
+void *s21_trim(const char *src, const char *trim_chars) {
+    int i = 0, j = s21_strlen(src);
+    char tp[2] = {*(char*)(src + i), '\0'};
+    while (s21_strpbrk(trim_chars, tp) != NULL) {
+         i++;
+        tp[0] = *(char*)(src + i);
+    }
+    tp[0] = *(char*)(src + j - 1);
+    while (s21_strpbrk(trim_chars, tp) != NULL) {
+        j--;
+        tp[0] = *(char*)(src + j);
+    }
+    void* temp = malloc((j - i) * sizeof(char)); 
+    s21_memcpy(temp, (char*)(src + i), j - i + 1);
+    return temp;
 }
--- src/test.c
+++ src/test.c
@@ -1,102 +1,70 @@
 #include <stdio.h>
 #include <string.h>
 #include "s21_string.h"
-
-int  ch = 'r';
-char str[] =    "lazy";
-char string[] = "The quick brown dog jumps over the lazy fox";
-char fmt1[] =   "         1         2         3         4         5";
-char fmt2[] =   "12345678901234567890123456789012345678901234567890";
+#include <stdlib.h>
 
 int main(void) {
-    char *pdest;
-    int result;
-    printf("String to be searched:\n             %s\n", string);
-    printf("             %s\n             %s\n\n", fmt1, fmt2);
-
-    printf("Search char: %c\n", ch);
-    pdest = s21_memchr(string, ch, sizeof(string));
-    result = (int)(pdest - string + 1);
-    if (pdest != NULL) {
-        printf("Result:      %c found at position %d\n", ch, result);
-    } else {
-        printf("Result:      %c not found\n", ch);
-    }
-    
-   char first[]  = "51111";
-   char second[] = "41111";
-   int int_arr1[] = {1,1,3,4};
-   int int_arr2[] = {1,2,3,4};
-
-   printf( "Compare '%.20s' to '%.20s':\n", first, second );
-   result = s21_memcmp( first, second, 5 );
-   printf("%d\n", result);
-   if( result < 0 )
-      printf( "First is less than second.\n" );
-   else if( result == 0 )
-      printf( "First is equal to second.\n" );
-   else
-      printf( "First is greater than second.\n" );
-
-   printf( "Compare '%d,%d' to '%d,%d':\n", int_arr1[0], int_arr1[1], int_arr2[0], int_arr2[1]);
-   result = s21_memcmp( int_arr1, int_arr2, sizeof(int) * 2 );
-   if( result < 0 )
-      printf( "int_arr1 is less than int_arr2.\n" );
-   else if( result == 0 )
-      printf( "int_arr1 is equal to int_arr2.\n" );
-   else
-      printf( "int_arr1 is greater than int_arr2.\n" );
-  
-  char str1[] = "Пример строки";
-  char str2[40];
-  char str3[60];
- 
-  s21_memcpy (str2, str1, strlen(str1)+1);               // копируем строку str1  и сохраняем в str2
-  s21_memcpy (str3, "Копирование успешно выполнено",60); // копируем 60 байт строки и сохраняем в str3
-  
-  printf("%s\n", str1);
-  printf("%s\n", str2);
-  printf("%s\n", str3);
-
-  // Исходный массив данных
-   unsigned char src2[10]="1234567890";
-
-   // Вывод массива src на консоль
-   printf ("src2 old: %s\n",src2);
-
-   // Копируем 3 байт
-   s21_memmove (&src2[4], &src2[3], 3);
-
-   // Вывод массива src на консоль
-   printf ("src2 new: %s\n",src2);
-  
-  // Исходный массив данных
-   unsigned char src[10]="1234567890";
-
-   // Вывод массива src на консоль
-   printf ("src old: %s\n",src);
-
-   // Копируем 3 байт
-   memmove (&src[4], &src[3], 3);
-
-   // Вывод массива src на консоль
-   printf ("src new: %s\n",src);
 
    // Исходный массив
-   unsigned char src21[15]="1234567890";
+   char src[22]="     Hello World-----\0";
+   char new_src[5]="* \'-\0";
  
-   // Заполняем массив символом ‘1’
-   memset (src21, '1', 10);
+/*    // Заполняем массив символом ‘1’
+   memset (src, 'a', 10);
 
    // Вывод массива src на консоль
-   printf ("src21: %s\n",src21);
+   printf ("src: %s-\n",src);
 
-   // Исходный массив
-   unsigned char src22[15]="1234567890";
- 
-   // Заполняем массив символом ‘1’
-   s21_memset (src22, '1', 10);
+   printf ("s21_strlen: %d\n",s21_strlen(src));
 
-   // Вывод массива src на консоль
-   printf ("src22: %s\n",src22);
+   printf ("s21_strchr: %s\n",s21_strchr(src, 108));
+
+   printf ("src: %s\n", src);
+
+   char* upper = (char*)s21_to_upper(src);
+
+   printf ("s21_to_upper: %s\n",upper);
+
+   free(upper);
+
+   printf ("src: %s\n", src);
+
+   char* lower = (char*)s21_to_lower(src);
+
+   printf ("s21_to_lower: %s\n", lower);
+
+   free(lower);
+
+   printf ("src: %s\n", src); */
+
+/*    printf ("src: %s\n", src);
+
+   printf ("src2: %s\n", new_src);
+
+   char* ins = (char*)s21_insert(src, new_src, 3);
+
+   printf ("ins: %s\n",ins);
+
+   printf ("src: %s\n", src);
+
+   printf ("src2: %s\n", new_src);
+
+   free(ins); */
+
+   printf ("src: %s\n", src);
+
+   printf ("src2: %s\n", new_src);
+
+   char* trim = (char*)s21_trim(src, new_src);
+
+   printf ("trim: -%s-\n",trim);
+
+   printf ("src: %s\n", src);
+
+   printf ("src2: %s\n", new_src);
+
+   printf ("src: %s\n", src);
+
+   free(trim);
+
 }
