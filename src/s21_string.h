bec316e3acac21ab60e5e4b609c8f0c0eebc5a76
camaerie <camaerie@student.21-school.ru>
2021-11-02 14:32:40 +0700

    strbrk, stRRchr tests

--- src/s21_string.h
+++ src/s21_string.h
@@ -23,4 +23,6 @@ char *s21_strcpy(char *dest, const char *src);
 
 char *s21_strpbrk(const char *str1, const char *str2);
 
+char *s21_strrchr(const char *str, int c);
+
 #endif  // SRC_S21_STRING_H_
---
+++ src/tests/tests.h
@@ -0,0 +1,9 @@
+#ifndef SRC_TESTS_TESTS_H_
+#define SRC_TESTS_TESTS_H_
+
+#include <check.h>
+
+TCase* CreateStrpbrkCase();
+TCase* CreateStrrchrCase();
+
+#endif  // SRC_TESTS_TESTS_H_
