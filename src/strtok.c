9ab9d4151f54cb80c890b151184376a7b1de2994
Narzaru <yaroslahek@gmail.com>
2021-11-03 16:49:44 +0700

    strtok.c hotfix

--- src/strtok.c
+++ src/strtok.c
@@ -1,35 +1,43 @@
 #include "s21_string.h"
 
 
+
 char *s21_strtok(char *str, const char *delim) {
   char *result;
   char *start_token;
   char *end_token;
-  static char *last_point;
+  static char* last_point;
 
-  if(str == S21_NULL) {
-    str = last_point;
-  }
-  if(*str == '\0') {
-    result = S21_NULL;
-  }
-
-  start_token = str + strspn(str, delim);
-  end_token = start_token;
-
-  if(*start_token == '\0') {
-    last_point = start_token;
-    result = S21_NULL;
+  if(str != S21_NULL) {
+    last_point = str;
+    start_token = last_point;
   } else {
-    last_point = start_token;
-    result = start_token;
+    start_token = last_point;
   }
 
-  end_token = start_token + (strpbrk(start_token, delim) - start_token);
-  if (end_token != S21_NULL) {
-    *end_token = '\0';
-    last_point = end_token + 1;
+  if(start_token != S21_NULL) {
+    start_token += strspn(start_token, delim); /* clear delim from start */
+    end_token = start_token;
+
+    if(*start_token == '\0') {
+      last_point = S21_NULL;
+      result = S21_NULL;
+    } else {
+      last_point = S21_NULL;
+      result = start_token;
+    }
+
+    end_token = start_token + (strpbrk(start_token, delim) - start_token);
+    if (end_token != S21_NULL) {
+      *end_token = '\0';
+      last_point = end_token + 1;
+    } else {
+      last_point = S21_NULL;
+      result = start_token;
+    }
+
   } else {
+    last_point = S21_NULL;
     result = S21_NULL;
   }
 
