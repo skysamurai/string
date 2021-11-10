6cc4be10f8d30440335973b00c4c533e95959467
pogozh <pogozh@gmail.com>
2021-11-10 21:58:49 +0700

    tests spn&cmp done

--- src/tests/strcmpCase.c
+++ src/tests/strcmpCase.c
@@ -65,50 +65,19 @@ START_TEST(arg2NULLTest) {  // must return SIGSEGV
 }
 END_TEST
 
-START_TEST(arg3TooMuchTest) {
-    int origResult = strcmp("aba", "aaa");
-    int s21Result = s21_strcmp("aba", "aaa");
-
-    ck_assert_msg(s21Result == origResult,
-                  "fail for strcmp(\"aba\", \"aaa\"). orig:%d,  s21:%d",
-                  origResult, s21Result);
-}
-END_TEST
-
-START_TEST(arg3ZeroTest) {
-    int origResult = strcmp("baa", "aaa");
-    int s21Result = s21_strcmp("baa", "aaa");
-
-    ck_assert_msg(s21Result == origResult,
-                  "fail for strcmp(\"baa\", \"aaa\", 0). orig:%d,  s21:%d",
-                  origResult, s21Result);
-}
-END_TEST
-
-START_TEST(arg3NULLTest) {  // must return SIGSEGV
-    int* nullint = S21_NULL;
-
-    s21_strcmp("aaa", "baa");
-}
-END_TEST
-
 TCase* CreateStrcmpCase() {
     TCase* strcmpCase = tcase_create("strcmpCase");
 
     tcase_add_test(strcmpCase, normalEqualTest);
     tcase_add_test(strcmpCase, normalLessTest);
     tcase_add_test(strcmpCase, normalMoreTest);
-    tcase_add_test(strcmpCase, arg3TooMuchTest);
-    tcase_add_test(strcmpCase, arg3ZeroTest);
     tcase_add_test(strcmpCase, arg1EmptyTest);
     tcase_add_test(strcmpCase, arg2EmptyTest);
 
     tcase_add_test_raise_signal(strcmpCase, arg1NULLTest, SIGSEGV);
     tcase_add_test_raise_signal(strcmpCase, arg2NULLTest, SIGSEGV);
-    tcase_add_test_raise_signal(strcmpCase, arg3NULLTest, SIGSEGV);
 
     return strcmpCase;
 }
 
 // arg Null
-
--- src/tests/strcspnCase.c
+++ src/tests/strcspnCase.c
@@ -6,20 +6,66 @@
 #include "tests.h"
 
 // TODO
-START_TEST(normalTest) {
-    char str[] = "fcba73";
-    char keys[] = "1234567890";
-    int i;
-    i = strcspn (str,keys);
-    int s21_i;
-    s21_i = s21_strcspn (str,keys);
-    ck_assert(i == s21_i);
+START_TEST(normalEqualTest) {
+    int origResult = strcspn ("1234567890","1234567890");
+    int s21Result = s21_strcspn ("1234567890","1234567890");
+    ck_assert_msg(s21Result == origResult, "fail for strcspn(\"1234567890\", \"1234567890\"). orig:%d,  s21:%d",
+                  origResult, s21Result);
+}
+END_TEST
+
+START_TEST(normalLessTest) {
+    int origResult = strcspn ("1234567890","fcba73");
+    int s21Result = s21_strcspn ("1234567890","fcba73");
+    ck_assert_msg(s21Result == origResult, "fail for strcspn(\"1234567890\", \"fcba73\"). orig:%d,  s21:%d",
+                  origResult, s21Result);
+}
+END_TEST
+
+START_TEST(normalMoreTest) {
+    int origResult = strcspn ("fcba73","1234567890");
+    int s21Result = s21_strcspn ("fcba73","1234567890");
+    ck_assert_msg(s21Result == origResult, "fail for strcspn(\"fcba73\", \"1234567890\"). orig:%d,  s21:%d",
+                  origResult, s21Result);
+}
+END_TEST
+
+START_TEST(arg1EmptyTest) {
+    int origResult = strcspn ("","1234567890");
+    int s21Result = s21_strcspn ("","1234567890");
+    ck_assert_msg(s21Result == origResult, "fail for strcspn(\"\", \"1234567890\"). orig:%d,  s21:%d",
+                  origResult, s21Result);
+END_TEST
+}
+
+START_TEST(arg2EmptyTest) {
+    int origResult = strcspn ("1234567890","");
+    int s21Result = s21_strcspn ("1234567890","");
+    ck_assert_msg(s21Result == origResult, "fail for strcspn(\"1234567890\", \"\"). orig:%d,  s21:%d",
+                  origResult, s21Result);
+   // printf(">>>>>>strspn arg1MOreTest original/s21 %d = %d \n ", origResult, s21Result);
+}
+END_TEST
+
+START_TEST(arg1NULLTest) {  // must return SIGSEGV
+    s21_strcspn (S21_NULL,"1234567890");
+}
+END_TEST
+
+START_TEST(arg2NULLTest) {  // must return SIGSEGV
+    s21_strcmp(S21_NULL, "aaa");
+    s21_strcspn ("1234567890",S21_NULL);
 }
 END_TEST
 
 TCase* CreateStrcspnCase() {
     TCase* strcspnCase = tcase_create("strcspn case");
-    tcase_add_test(strcspnCase, normalTest);
-
+    tcase_add_test(strcspnCase, normalEqualTest);
+    tcase_add_test(strcspnCase, normalLessTest);
+    tcase_add_test(strcspnCase, normalMoreTest);
+    tcase_add_test(strcspnCase, arg1EmptyTest);
+    tcase_add_test(strcspnCase, arg2EmptyTest);
+    tcase_add_test_raise_signal(strcspnCase, arg1NULLTest, SIGSEGV);
+    tcase_add_test_raise_signal(strcspnCase, arg2NULLTest, SIGSEGV);
     return strcspnCase;
 }
