1c973edc1523c2fd14178bcfac82f5d6d140915e
camaerie <camaerie@student.21-school.ru>
2021-11-15 17:34:39 +0700

    after merge fix

--- src/tests/memmoveCase.c
+++ src/tests/memmoveCase.c
@@ -8,8 +8,8 @@
 START_TEST(normalEqualTest) {
     char str1[100] = "aaa";
     char str2[100] = "aaa";
-    char*  origResult = memmove(str1, "aaa", 3);
-    char*  s21Result = s21_memmove(str2, "aaa", 3);
+    char* origResult = memmove(str1, "aaa", 3);
+    char* s21Result = s21_memmove(str2, "aaa", 3);
 
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"aaa\", \"aaa\", 3). orig:%s,  s21:%s",
@@ -17,13 +17,11 @@ START_TEST(normalEqualTest) {
 }
 END_TEST
 
-
-
 START_TEST(normalLessTest) {
     char str1[100] = "aaa";
     char str2[100] = "aaa";
-    char*  origResult = memmove(str1, "12", 3);
-    char*  s21Result = s21_memmove(str2, "12", 3);
+    char* origResult = memmove(str1, "12", 3);
+    char* s21Result = s21_memmove(str2, "12", 3);
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"aaa\", \"123\",3). orig:%s,  s21:%s",
                   origResult, s21Result);
@@ -33,8 +31,8 @@ END_TEST
 START_TEST(normalMoreTest) {
     char str1[100] = "aaa";
     char str2[100] = "aaa";
-    char*  origResult = memmove(str1, "12345", 3);
-    char*  s21Result = s21_memmove(str2, "12345", 3);
+    char* origResult = memmove(str1, "12345", 3);
+    char* s21Result = s21_memmove(str2, "12345", 3);
 
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"aba\", \"12345\", 3). orig:%s,  s21:%s",
@@ -42,11 +40,11 @@ START_TEST(normalMoreTest) {
 }
 END_TEST
 
-START_TEST(arg1EmptyTest) { 
-char str1[100] = "\0";
-char str2[100] = "\0";
-    char*  origResult = memmove(str1, "aaa", 3);
-    char*  s21Result = s21_memmove(str2, "aaa", 3);
+START_TEST(arg1EmptyTest) {
+    char str1[100] = "\0";
+    char str2[100] = "\0";
+    char* origResult = memmove(str1, "aaa", 3);
+    char* s21Result = s21_memmove(str2, "aaa", 3);
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"\", \"aaa\", 3). orig:%s,  s21:%s",
                   origResult, s21Result);
@@ -56,8 +54,8 @@ END_TEST
 START_TEST(arg2EmptyTest) {
     char str1[100] = "aaa";
     char str2[100] = "aaa";
-    char*  origResult = memmove(str1, "", 1);
-    char*  s21Result = s21_memmove(str2, "", 1);
+    char* origResult = memmove(str1, "", 1);
+    char* s21Result = s21_memmove(str2, "", 1);
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"aaa\", \"\", 3). orig:%s,  s21:%s",
                   origResult, s21Result);
@@ -67,8 +65,8 @@ END_TEST
 START_TEST(arg3TooMuchTest) {
     char str1[1000] = "aaa";
     char str2[1000] = "aaa";
-    char*  origResult = memmove(str1, "123", 4);
-    char*  s21Result = s21_memmove(str2, "123", 4);
+    char* origResult = memmove(str1, "123", 4);
+    char* s21Result = s21_memmove(str2, "123", 4);
 
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"aaa\", \"123\", 333). orig:%s,  s21:%s",
@@ -79,8 +77,8 @@ END_TEST
 START_TEST(arg3ZeroTest) {
     char str1[100] = "aaa";
     char str2[100] = "aaa";
-    char*  origResult = memmove(str1, "123", 0);
-    char*  s21Result = s21_memmove(str2, "123", 0);
+    char* origResult = memmove(str1, "123", 0);
+    char* s21Result = s21_memmove(str2, "123", 0);
 
     ck_assert_msg(strcmp(s21Result, origResult) == 0,
                   "fail for memmove(\"aaa\", \"123\", 0). orig:%s,  s21:%s",
@@ -99,5 +97,5 @@ TCase* CreateMemmoveCase() {
     tcase_add_test(memmoveCase, arg1EmptyTest);
     tcase_add_test(memmoveCase, arg2EmptyTest);
 
-return memmoveCase;
+    return memmoveCase;
 }
